8a7f49f7797385f146f6046da440b1a6e260d026
YakeshPC <rakhesh.stp@gmail.com>
2020-01-27 19:30:17 +0530

    creating

---
+++ src/BasicPros/WelCome.java
@@ -0,0 +1,28 @@
+package BasicPros;
+
+public class WelCome {
+	
+	public static void main(String[] args) {
+		
+			int apps=50;
+			int tech=80;
+			
+			if(apps>=40){
+				System.out.println("First Round CLear");
+				if(tech>=75){
+					System.out.println("select for hr round");
+				}
+				else{
+					System.out.println("Failed in Technical");
+				}
+			}
+			else{
+				System.out.println("Atttend the interview after 6 Months");
+			}
+					
+
+
+				
+				
+	}
+}
